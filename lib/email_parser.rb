062920bd6a85150235add17dc4a60931d004961e
Evan Burton <evan@heyevan.com>
2020-05-09 18:01:29 +0000

    Automatically backed up by Learn

--- lib/email_parser.rb
+++ lib/email_parser.rb
@@ -14,7 +14,7 @@ def parse
   #   email.include?(" ")
   #   @email.split(" ")
   # elsif @email.include?(", ") && @email.include?(" ")
-@email.split(/[\s , ]/).delete_at(3)
+@email.split(/[\s , ]/).delete_at(1)
 # else
 #   end
 end
